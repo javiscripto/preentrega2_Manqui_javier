1751540d0900a70f84153d2201cc60bd03124419
javiscripto <javier.mecker94@gmail.com>
2023-06-26 20:53:25 -0400

    primer commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+function Navbar(){
+    return(
+        
+    <nav>
+        <h1>GuitarraStore</h1>
+        <ul>
+            <li><a href="#">inicio</a></li>
+            <li><a href="#">productos</a></li>
+            <li><a href="#">contacto</a></li>
+        </ul>
+    </nav>        
+        
+    )
+}
+export default Navbar
